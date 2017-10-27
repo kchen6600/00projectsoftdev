5e24455aeba9d86862790dd4dad4bb531ac886ee
Sasha Fomina <afomina@stuy.edu>
2017-10-27 11:07:33 -0400

    stories

--- story.py
+++ story.py
@@ -1,20 +1,29 @@
 from flask import Flask, render_template
 import sqlite3
 from utils import dbLibrary
+import hashlib
 
 story_app = Flask(__name__)
 
 #Creating the database 
 openDB("data/stories.db")
 cursor = createCursor(db)
+
 createTable(mainStories,['title','storyID', 'timeLast', 'lastAdd','storyFile','lastEditor'] ,['TEXT', 'INTEGER PRIMARY KEY AUTOINCREMENT','datetime2', 'TEXT', 'TEXT', 'TEXT'])
 
 createTable (userStories, ['username', 'storyIDs' , 'myAddition'], ['TEXT' , 'TEXT' , 'TEXT'])
 
-            
-@story_app.route("/")
+createTable(accounts, ['username', 'password'], ['TEXT', 'TEXT'])
+
+
+@story_app.route("/", methods = ['POST', 'GET'])
 def login():
     return render_template("login.html")
+
+@story_app.route("/createAccount", methods = ['POST', 'GET'])
+def createAccount():
+    
+
 
 @story_app.route("/home",methods = ['POST','GET'])
 def homepage():
