8de43f85f0af4b520f201ab4a77f24d2d887ec73
sonalparab <sparab@stuy.edu>
2017-10-26 22:30:52 -0700

    Account creationtemplate

--- story.py
+++ story.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import sqlite3
-from utils import dbLibrary
+#from utils import dbLibrary
 
 story_app = Flask(__name__)
 
-#Creating the database 
+
+'''#Creating the database 
 openDB("data/stories.db")
 cursor = createCursor(db)
 createTable(mainStories,['title','storyID', 'timeLast', 'lastAdd','storyFile','lastEditor'] ,['TEXT', 'INTEGER PRIMARY KEY AUTOINCREMENT','datetime2', 'TEXT', 'TEXT', 'TEXT'])
 
-createTable (userStories, ['username', 'storyIDs' , 'myAddition'], ['TEXT' , 'TEXT' , 'TEXT'])
+createTable (userStories, ['username', 'storyIDs' , 'myAddition'], ['TEXT' , 'TEXT' , 'TEXT'])'''
 
             
 @story_app.route("/")
+def root():
+    return redirect(url_for('login'))
+
+@story_app.route("/login",methods = ['POST','GET'])
 def login():
     return render_template("login.html")
+
+@story_app.route("/account")
+def account():
+    return render_template("account.html")
 
 @story_app.route("/home",methods = ['POST','GET'])
 def homepage():
